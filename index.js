d97988594b067b1971194480b3c1e3034fdf4b3b
regiencinares <96848420+regiencinares@users.noreply.github.com>
2026-03-13 16:20:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  return new Response(JSON.stringify({ message: "Hello from Cloudflare Worker!" }), {
-    headers: { 
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*" // This allows your HTML to read the data
-    }
-  });
-}
+export default {
+  async fetch(request, env, ctx) {
+    return new Response(JSON.stringify({ 
+      message: "Hello from Cloudflare Worker!",
+      status: "Build Successful"
+    }), {
+      headers: { 
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*" 
+      }
+    });
+  },
+};
